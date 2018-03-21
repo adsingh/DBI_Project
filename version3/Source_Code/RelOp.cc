36a737fb5e1f6b5b7d7cd1ddd41d1848d98cc35b
Ajinkya Gaikwad <ajinkyagaikwad2709@gmail.com>
2018-03-21 14:09:15 -0400

    Implemented SelectFile

--- version3/Source_Code/RelOp.cc
+++ version3/Source_Code/RelOp.cc
@@ -93,12 +93,93 @@ void *SelectPipe::selectPipeWorker(void *args) {
 
 void SelectFile::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
 	cout << "[SelectFile][Run] Start" << endl;
+	pthread_attr_t attr;
+	int retVal;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	
+	workerArgs.inFile = &inFile;
+	workerArgs.outPipe = &outPipe;
+	workerArgs.selOp = &selOp;
+	workerArgs.literal = &literal;
+	workerArgs.totalPages = totalPages == 0 ? 1 : totalPages;
+
+	retVal = pthread_create(&worker, &attr, selectFileWorker, (void *) &workerArgs);
+	cout << "[SelectFile][Run] Created new thread" << endl;
+	if(retVal) {
+		cout << "[SelectFile][Run] Error in pthread_create" << endl;
+		exit(1);
+	}
+	pthread_attr_destroy(&attr);
+	cout << "[SelectFile][Run] End" << endl;
 }
 
 void SelectFile::WaitUntilDone () {
-	// pthread_join (thread, NULL);
+	void* status;
+	int retVal;
+	retVal = pthread_join(worker, &status);
+	if(retVal) {
+		cout << "[SelectFile][WaitUntilDone] Error in pthread_join" << endl;
+		exit(1);
+	}
 }
 
 void SelectFile::Use_n_Pages (int runlen) {
-
+	totalPages = runlen;
 }
+
+void *SelectFile::selectFileWorker(void *args) {
+	cout << "[SelectFile][selectFileWorker] Start" << endl;
+
+	thread_data *worker_args = (thread_data *) args;
+	Page* pageArr[worker_args->totalPages]; 
+	// Initialize all Pages
+	for(int i = 0; i < worker_args->totalPages; i++) {
+		pageArr[i] = new Page();
+	}
+
+	worker_args->inFile->MoveFirst();
+	// Indicates the no of pages filled
+	int currentPageNo = 0;
+	Record* record = new Record();
+	Record* tempRecord = new Record();
+	ComparisonEngine comp;
+
+
+	while(worker_args->inFile->GetNext(*record, *worker_args->selOp, *worker_args->literal) == 1) {
+		// Add to current page in page array
+		if(pageArr[currentPageNo]->Append(record) == 0) {
+			currentPageNo++;
+			// If all pages in page array are full, flush into output pipe
+			if(currentPageNo == worker_args->totalPages) {
+				for(int i = 0; i < worker_args->totalPages; i++) {
+					while(pageArr[i]->GetFirst(tempRecord) == 1) {
+						worker_args->outPipe->Insert(tempRecord);
+					}
+				}
+				currentPageNo = 0;
+			}
+			pageArr[currentPageNo]->Append(record);
+		}
+	}
+	
+	// Insert the remaining records into output pipe
+	for(int i = 0; i <= currentPageNo; i++) {
+		while(pageArr[i]->GetFirst(tempRecord) == 1) {
+			worker_args->outPipe->Insert(tempRecord);
+		}
+	}
+
+	for(int i = 0; i < worker_args->totalPages; i++) {
+		delete pageArr[i];
+	}
+	delete record;
+	delete tempRecord;
+	
+	worker_args->outPipe->ShutDown();
+
+	cout << "[SelectFile][selectFileWorker] End" << endl;
+}
+
+
